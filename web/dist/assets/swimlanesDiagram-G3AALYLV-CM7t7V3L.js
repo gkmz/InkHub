63d396022550bac04c0ce2658e97aac0fd0f204d
hankmo <hankmo.x@gmail.com>
2026-08-02 00:21:28 +0800

    build(web): refresh embedded frontend assets

---
+++ web/dist/assets/swimlanesDiagram-G3AALYLV-CM7t7V3L.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-D-aKGq1-.js";import{_ as a}from"./mermaid.core-CvtchVY6.js";import"./chunk-5VM5RSS4-BbBKoMCU.js";import"./chunk-XXDRQBXY-Bk7KzuYG.js";import"./chunk-VR4S4FIN-DOD_SvNE.js";import"./chunk-32BRIVSS-FKugHN0d.js";import"./channel-DgdOjdiK.js";import"./index-s95HrOoY.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
