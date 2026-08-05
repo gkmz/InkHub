3006416efe9d72305aa2c620a818e0e6ba914201
hankmo <hankmo.x@gmail.com>
2026-08-05 17:48:01 +0800

    remove dist

--- web/dist/assets/swimlanesDiagram-G3AALYLV-C2ss9JC0.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-Bbj64kC1.js";import{_ as a}from"./mermaid.core-BAe_XMN7.js";import"./chunk-5VM5RSS4-D5xU3JY1.js";import"./chunk-XXDRQBXY-DmsYB9fp.js";import"./chunk-VR4S4FIN-BK9L8aYr.js";import"./chunk-32BRIVSS-BbibxkFw.js";import"./channel-Ce-AA38K.js";import"./index-CL6lQsOQ.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
